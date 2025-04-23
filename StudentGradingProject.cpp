0a597b92c3a8ca7a43b31eeb7776b30bb7cb8ae3
rhyswood <rhyswood@RHYSWOOD4602>
2025-04-22 17:30:33 -0700

    Add project files.

---
+++ StudentGradingProject.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <limits>
+
+int main()
+{
+    double totalScore = 0;
+    int highScore = 0;
+    std::string winner;
+    //Ask for number of students
+    std::cout << "Please Specify number of students: ";
+    int numberOfStudents;
+    //save that input as an int
+    std::cin >> numberOfStudents;
+    //Ignore everything in the input buffer until we hit a newline character.
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //loop for that many times, asking for a student name and grade each time
+    for (int i = 0; i < numberOfStudents; i++) {
+        std::cout << "Please enter student name: ";
+        std::string studentName;
+        std::getline(std::cin, studentName);
+
+        std::cout << "Please enter " << studentName << "'s score: ";
+        int score;
+        std::cin >> score;
+        totalScore += score;
+        if (score > highScore) {
+            highScore = score;
+            winner = studentName;
+        }
+        //print Pass or Failif over or under 50
+        if (score < 50) {
+            std::cout << studentName << " failed.\n";
+        }
+        else {
+            std::cout << studentName << " passed! woooooo!\n";
+        }
+        // Clear newline after entering score, in case we use getline again
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    }
+    std::cout << "The class average is " << (totalScore / numberOfStudents) << "\n";
+    std::cout << "The highest score was " << highScore << " from " << winner << "!";
+    return 0;
+}
